23949a24df75965ef75edb2e734e97a70972ed0e
Zachary Durr <zdurr22@gmail.com>
2020-09-29 19:35:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(input)
-  if input.upcase == false
+  if input.upcase == input
     return "HUH?! SPEAK UP, SONNY!"
   elsif input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
